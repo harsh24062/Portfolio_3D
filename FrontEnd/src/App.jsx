7171cb79804176f37b95ad5f8cff6b1ec8c1cb16
harsh24062 <129416678+harsh24062@users.noreply.github.com>
2025-09-16 15:43:21 +0530

    Added Components, Assests, Constant, Style

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,8 +1,10 @@
 
-function App() {
-  return <div>
-   
-  </div>
+export default function App() {
+  return (
+    <div className="bg-hero-pattern bg-cover bg-center h-screen flex items-center justify-center">
+      <h1 className="text-primary text-4xl font-bold shadow-card">
+        Hello 3D Portfolio 🚀
+      </h1>
+    </div>
+  )
 }
-
-export default App
